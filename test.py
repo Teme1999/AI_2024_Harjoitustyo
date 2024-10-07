48aa995d33324da51468078907df308282d0ec9b
Teme1999 <teme.pee@gmail.com>
2024-10-07 15:57:14 +0300

    asd

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 import torch
-torch.cuda.is_available()
+
+# Check if CUDA is available
+if torch.cuda.is_available():
+    print(f"Using GPU: {torch.cuda.get_device_name(torch.cuda.current_device())}")
+else:
+    print("No GPU available, using CPU.")
